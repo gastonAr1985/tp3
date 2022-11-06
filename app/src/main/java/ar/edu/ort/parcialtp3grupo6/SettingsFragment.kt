db2c719f10170491a15b2b69ee4e56f6843df4f3
gastonAr1985 <gastonarbert@gmail.com>
2022-11-06 20:19:29 -0300

    first commit

---
+++ app/src/main/java/ar/edu/ort/parcialtp3grupo6/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package ar.edu.ort.parcialtp3grupo6
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.setting, rootKey)
+    }
+}
